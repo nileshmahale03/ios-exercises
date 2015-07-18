92525211f494dbc05fd955bbb07a47695572ad3e
nileshmahale <nileshmahale03@gmail.com>
2015-07-17 23:42:43 -0700

    Assignement 15 completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,56 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    
+    // we can only do arithmetic opration on NSInteger
+    // so convert NSNumber to NSInteger, do mupltiplication and convert back
+    NSInteger myInt = [number intValue];
+    NSNumber *twiceNumber = [NSNumber numberWithInt:(int)myInt*2];
+    
+    return twiceNumber;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    
+    // Array requires an object, so convert NSInteger to NSNumber before adding to array
+    
+    NSMutableArray *myArray = [[NSMutableArray alloc] init];
+    
+    for (NSInteger idx = number; idx <= otherNumber; idx ++) {
+        NSNumber *myNumber = [NSNumber numberWithInt:(int)idx];
+        [myArray addObject:myNumber];
+    }
+    
+    return myArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    
+    NSMutableArray *mutableArrayOfNumbers = [arrayOfNumbers mutableCopy];
+    
+    //First order the array in asecnding order
+    [mutableArrayOfNumbers sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        
+        
+        NSNumber *numberA = (NSNumber *)obj1;
+        NSNumber *numberB = (NSNumber *)obj2;
+        
+        int intValueA = [numberA intValue];
+        int intvalueB = [numberB intValue];
+        
+        if (intValueA > intvalueB) {
+            return NSOrderedDescending;
+        } else if (intValueA < intvalueB) {
+            return NSOrderedAscending;
+        }
+        return NSOrderedSame;
+    }];
+    
+    //element 0 will be the lowest number
+    
+    NSNumber *lowestNumber = mutableArrayOfNumbers[0];
+    
+    return [lowestNumber intValue];
 }
 
 @end
